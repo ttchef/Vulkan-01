53e21bca53908f6e64b7e4770fe479d12a3e4a1a
ttchef <ruro0.edit@gmail.com>
2025-07-09 16:39:29 +0200

    improvment

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -std=c++17 -O2
 LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 
 VulkanTest: main.cpp
-	g++ $(CLAGS) -o VulkanTest main.cpp $(LDFLAGS)
+	g++ $(CFLAGS) -o VulkanTest main.cpp $(LDFLAGS)
 
 .PHONY: test clean
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 
-#include <exception>
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream> 
 #include <stdexcept>
 #include <cstdlib> 
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanUp();
@@ -16,16 +20,39 @@ public:
 
 private:
 
+    GLFWwindow* window;
+
+    void initWindow() {
+        if (!glfwInit()) {
+            std::cerr << "Failed to initialise GLFW!\n";
+            return;
+        }
+    
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        if (!window) {
+            std::cerr << "Failed to create a window!\n";
+            return;
+        }
+
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanUp() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 
 };
@@ -37,6 +64,7 @@ int main() {
     try {
         app.run();
     } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
